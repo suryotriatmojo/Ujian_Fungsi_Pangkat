2ec9875a7d84f9cd1d6d4b0eae7c5f5e7e9b834a
suryotriatmojo <atmojo.suryo@gmail.com>
2019-05-03 14:03:34 +0700

    fungsi pangkat

---
+++ ujian_fungsi_pangkat.py
@@ -0,0 +1,16 @@
+# membuat function pangkat
+def pangkat(a,b):
+    a = int(a)
+    kali = int(a)
+    i = 1
+    while i < int(b):
+        kali = kali * a
+        i += 1
+    return kali
+    
+print(pangkat(2,2))
+print(pangkat(3,3))
+print(pangkat(10,5))
+
+# just in case menggunakan input untuk parameter a & b
+# print(pangkat(input('Masukkan angka pertama: '), input('Masukkan angka kedua: ')))
